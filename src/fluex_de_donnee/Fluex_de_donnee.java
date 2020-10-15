6a78e8ad1146587c1dac03183c6f814ec6d1bf82
mohammedbelaidi <mbilaidi@gmail.com>
2020-10-15 18:15:12 +0100

    fluxe d donnes

---
+++ src/fluex_de_donnee/Fluex_de_donnee.java
@@ -0,0 +1,22 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package fluex_de_donnee;
+import fluex_de_donnee.CS;
+/**
+ *
+ * @author MOHAMMED-PC
+ */
+public class Fluex_de_donnee {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        new CS().show();
+    }
+    
+}
